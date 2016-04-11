702026fed2944e83b316f5a551c7651ea6634272
Robert C. Martin <unclebob@cleancoder.com>
2016-04-11 12:58:20 -0500

    MACS E1 T2 -- building the board

--- Gomoku/Board.swift
+++ Gomoku/Board.swift
@@ -1,6 +1,23 @@
 
+enum Player {
+    case White
+    case Black
+    case Empty
+}
+
 class Board {
+    var stones = 0
+    var placedStones = [Int: Player]()
+    
     func stonesPlaced() -> Int {
-        return -1
+        return stones
+    }
+    
+    func add(row: Int, column: Int, player: Player) {
+        stones += 1
+    }
+    
+    func get(row: Int, column: Int) -> Player {
+        return .White
     }
 }
--- GomokuTests/BoardTest.swift
+++ GomokuTests/BoardTest.swift
@@ -4,10 +4,32 @@ import XCTest
 @testable import Gomoku
 
 class BoardTest: XCTestCase {
-    func testCanCreateBoard() {
-        var board = Board()
+    var board: Board!
+    
+    override func setUp() {
+        super.setUp()
+        board = Board()
+    }
+    
+    func testNewBoardHasNoStones() {
         let stones = board.stonesPlaced()
         XCTAssertEqual(0, stones)
+    }
+ 
+    func testCanAddOneWhiteStone() {
+        let row = 1
+        let col = 1
+        let player = Player.White
+        board.add(row, column: col, player: player)
+        
+        XCTAssertEqual(1, board.stonesPlaced())
+        
+        let placedStone = board.get(row, column:col)
+        XCTAssertEqual(Player.White, placedStone)
+    }
+    
+    func testKnowsAboutEmptyIntersections() {
+        XCTAssertEqual(Player.Empty, board.get(0,column: 1))
     }
     
 }
